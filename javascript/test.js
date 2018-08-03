20368bd62ca0661b16ed2f70c5151e581814cbb6
Javier González de Lope <jglope@visiotechsecurity.com>
2018-08-03 15:21:22 +0200

    Toolbar buttons functionality added.

--- javascript/test.js
+++ javascript/test.js
@@ -1,13 +1,20 @@
 var canvas;
 
+var white_rect;
+var external_border;
+var internal_border;
+
+fabric.Object.prototype.objectCaching = false
+
 function initCanvas() {
 
     canvas.setBackgroundColor('red', canvas.renderAll.bind(canvas))
     canvas.setHeight(570);
     canvas.setWidth(1080);
+    canvas.preserveObjectStacking = true;
 
     //Rectángulo que representa la tarjeta (blanca)
-    var white_rect = new fabric.Rect({
+    white_rect = new fabric.Rect({
         width: 636,
         height: 406,
         selectable: false,
@@ -21,7 +28,7 @@ function initCanvas() {
     canvas.centerObject(white_rect);
 
     //Borde de seguridad externo de la tarjeta
-    var external_border = new fabric.Rect({
+    external_border = new fabric.Rect({
         width: 636,
         height: 406,
         selectable: false,
@@ -37,7 +44,7 @@ function initCanvas() {
     canvas.centerObject(external_border);
 
     //Borde de seguridad interno de la tarjeta
-    var internal_border = new fabric.Rect({
+    internal_border = new fabric.Rect({
         width: external_border.get('width') - 14.57 + 2,
         height: external_border.get('height') - 14.57 + 2,
         selectable: false,
@@ -65,6 +72,8 @@ function initCanvas() {
 
     canvas.on('selection:cleared', function () {
         $('#info').text('No se ha seleccionado ningún elemento.')
+        $('#object-controls').addClass('hidden')
+        $('#txt-controls').addClass('hidden')
     })
 
     canvas.renderAll();
@@ -74,17 +83,69 @@ function getSelection(){
     var selected = canvas.getActiveObject();
     if (selected) {
         var type = selected.get('type');
+        $('#object-controls').removeClass('hidden')
         if (type === 'activeSelection') {
             $('#info').text('Type: multiple selection')
+            $('#txt-controls').addClass('hidden')
         } else {
             $('#info').text('Type: ' + type)
+            if (type === 'i-text'){
+                $('#txt-controls').removeClass('hidden')
+            }else{
+                $('#txt-controls').addClass('hidden')
+            }
         }
 
     }
 }
 
+function copy() {
+    canvas.getActiveObject().clone(function (cloned) {
+        _clipboard = cloned;
+    });
+}
+
+function paste() {
+    // clone again, so you can do multiple copies.
+    _clipboard.clone(function (clonedObj) {
+        canvas.discardActiveObject();
+        clonedObj.set({
+            left: clonedObj.left + 10,
+            top: clonedObj.top + 10,
+            evented: true,
+        });
+        if (clonedObj.type === 'activeSelection') {
+            // active selection needs a reference to the canvas.
+            clonedObj.canvas = canvas;
+            clonedObj.forEachObject(function (obj) {
+                canvas.add(obj);
+            });
+            // this should solve the unselectability
+            clonedObj.setCoords();
+        } else {
+            canvas.add(clonedObj);
+        }
+        _clipboard.top += 10;
+        _clipboard.left += 10;
+        canvas.setActiveObject(clonedObj);
+        canvas.requestRenderAll();
+    });
+}
+
+function deleteElement() {
+    var selected = canvas.getActiveObjects();
+    if (selected) {
+        //if (confirm('¿Está seguro de que desea borrar el elemento seleccionado?')) {
+        canvas.remove(...selected);
+        //}
+    }
+    canvas.discardActiveObject().renderAll();
+}
+
 jQuery(document).ready(function () {
     canvas = new fabric.Canvas('canvas');
+    $('#object-controls').addClass('hidden')
+    $('#txt-controls').addClass('hidden')
     initCanvas();
 
     /* ---------- AÑADIR UNA IMAGEN ---------- */
@@ -123,29 +184,14 @@ jQuery(document).ready(function () {
         var text = new fabric.IText('Tap and Type', {
             fontFamily: 'times new roman',
             fill: '#333',
-            fontSize: 14,
+            fontSize: 20,
             top: 400,
             left: 300
         });
 
         canvas.add(text);
-    })
-
-    jQuery('#add-ugly-text-btn').click(function () {
-
-        var text = new fabric.Text('Tap and Type', {
-            fontFamily: 'times new roman',
-            fill: '#333',
-            fontSize: 14,
-            top: 400,
-            left: 300
-        });
-
-        canvas.add(text);
-        //canvas.centerObject(text);
-        text.center();
+        canvas.centerObject(text)
         text.setCoords();
-        canvas.renderAll();
     })
 
     jQuery('#json-customization-btn').click(function () {
@@ -160,52 +206,88 @@ jQuery(document).ready(function () {
     })
 
     jQuery("#bold-btn").click(function () {
-
+        if ($(this).prop('checked') == true){
+            canvas.getActiveObject().set("fontWeight", "bold");
+        }else{
+            canvas.getActiveObject().set("fontWeight", "");
+        }
+        
+        canvas.renderAll();
     })
 
     jQuery("#italic-btn").click(function () {
-
+        
+        if ($(this).prop('checked') == true) {
+            canvas.getActiveObject().set("fontStyle", "italic");
+        } else {
+            canvas.getActiveObject().set("fontStyle", "");
+        }
+        canvas.renderAll();
     })
 
     jQuery("#underline-btn").click(function () {
+        
+        if ($(this).prop('checked') == true) {
+            canvas.getActiveObject().set("underline", true);
+        } else {
+            canvas.getActiveObject().set("underline", false);
+        }
+        canvas.renderAll();
+    })
 
+    jQuery('#color-picker').change(function () {
+        canvas.getActiveObject().setFill(this.value);
+        canvas.renderAll();
+        jQuery(this).val("")
+    })
+
+    jQuery("#font-select").change(function(){
+        canvas.getActiveObject().setFontFamily(this.value);
+        canvas.renderAll();
+    })
+
+    jQuery("#size-select").change(function () {
+        canvas.getActiveObject().setFontSize(this.value);
+        canvas.renderAll();
     })
 
     jQuery("#copy-btn").click(function () {
-
+        copy();
     })
 
     jQuery("#paste-btn").click(function () {
-
+        paste();
     })
 
     jQuery("#delete-btn").click(function () {
-
+        deleteElement();
     })
 
     jQuery("#send-back-btn").click(function () {
-
+        canvas.getActiveObject().sendToBack();
+        white_rect.sendToBack();
+        canvas.renderAll();
     })
 
     jQuery("#bring-front-btn").click(function () {
-
+        canvas.getActiveObject().bringToFront();
+        external_border.bringToFront();
+        internal_border.bringToFront();
+        canvas.renderAll();
     })
 
     jQuery("#left-align-btn").click(function () {
-
+        canvas.getActiveObject().setTextAlign("left");
+        canvas.renderAll();
     })
 
     jQuery("#right-align-btn").click(function () {
-
+        canvas.getActiveObject().setTextAlign("right");
+        canvas.renderAll();
     })
 
     jQuery("#center-align-btn").click(function () {
-
+        canvas.getActiveObject().setTextAlign("center");
+        canvas.renderAll();
     })
-
-    jQuery("#get-info-btn").click(function () {
-
-    })
-
-    
 })
